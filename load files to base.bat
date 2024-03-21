6ec285d8588be79c971d86ae816dc9539d95a5d4
Лукин Сергей Викторович <lukin.sv@Domain.local>
2024-03-21 11:22:12 +0300

    Добавил пустую тестовую базу 1С. В батнике load files to base.bat прописал относительный путь к этой базе.

--- load files to base.bat
+++ load files to base.bat
@@ -1,3 +1,3 @@
 chcp 1251
-@set catalog=C:\Users\lukin.sv\Documents\test_git
+@set catalog=.\test_git
 "C:\Program Files\1cv8\8.3.23.1865\bin\1cv8.exe" CONFIG /F "C:\Users\lukin.sv\Documents\test_git"  /LoadConfigFromFiles "%catalog%" -listFile "diff_new.txt" /UpdateDBCfg
