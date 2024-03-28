a40f7b50594193aa4b4aa8a74f3cf3a4b602b57b
Durtanyan <71310816+Durtanyan@users.noreply.github.com>
2024-03-28 11:57:28 +0300

    Update load files to base.bat

--- load files to base.bat
+++ load files to base.bat
@@ -1,3 +1,3 @@
 chcp 1251
-@set catalog=.\test_git
+@set catalog=./test_git
 "C:\Program Files\1cv8\8.3.23.1865\bin\1cv8.exe" DESIGNER /F "C:\Users\lukin.sv\Documents\test_git"  /DisableStartupMessages /DisableStartupDialogs /LoadConfigFromFiles "%catalog%" -listFile "diff_new.txt" /UpdateDBCfg
